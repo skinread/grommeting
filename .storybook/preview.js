c208d731d22ed6f7c1c11a876899a305797e5afa
Sean Kinread <sean.kinread@tshirtventures.com.au>
2021-07-28 16:34:20 +1000

    Setup fresh Storybook with Grommet sample

---
+++ .storybook/preview.js
@@ -0,0 +1,24 @@
+export const parameters = {
+  layout: 'fullscreen',
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      // color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  // options: {
+  //   storySort: {
+  //     method: 'alphabetical',
+  //     order: ['All'],
+  //   },
+  // },
+}
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+  },
+};
